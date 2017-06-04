f4a55f1bad91bffa1cfce786316a709509b1cbaf
Tracey <traceyle@outlook.com>
2017-06-04 16:12:24 +1000

    Refactor save_file

--- save_file.py
+++ save_file.py
@@ -7,8 +7,9 @@ def save_file(json_data, project_uuid):
     json_data is a Python object, project_uuid is a string
     '''
 
-    # Open a project to write the data to
-    with open(os.path.join('projects/', project_uuid + '.json'), 'w') as project_file:
+    project_file_path = os.path.join('projects/', project_uuid + '.json')
+    # Write the project data to a file
+    with open(project_file_path, 'w') as project_file:
         json.dump(obj=json_data, fp=project_file, ensure_ascii=False)
 
 
